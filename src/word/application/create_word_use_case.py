0c83fa793c739039045522151e1aba513489fc7e
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-08-08 13:30:37 +0200

    Pass happy path

--- src/word/application/create_word_use_case.py
+++ src/word/application/create_word_use_case.py
@@ -1,5 +1,10 @@
+from src.shared.application.application_exceptions import ApplicationException
+from src.shared.domain.domain_exceptions import DomainException
 from src.word.application.stored_word_dto import StoredWordDto
+from src.word.domain.stored_word import StoredWord
+from src.word.domain.stored_word_factory import StoredWordFactory
 from src.word.domain.stored_word_repository import StoredWordRepository
+
 
 class CreateWordUseCase:
     def __init__(
@@ -12,4 +17,21 @@ class CreateWordUseCase:
         self,
         word_dto: StoredWordDto,
     ) -> StoredWordDto:
-        pass
+        try:
+            stored_word: StoredWord = StoredWordFactory.build(
+                word_dto.word,
+                word_dto.position,
+            )
+            await self.__stored_word_repository.save(
+                stored_word,
+            )
+            return StoredWordDto(
+                word=stored_word.word,
+                position=stored_word.position,
+            )
+
+        except DomainException as domain_exception:
+            raise ApplicationException(
+                domain_exception.standard_exception,
+                domain_exception.exception_message,
+            )
