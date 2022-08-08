244e3d882504839f98ec08927bccb59471b42b1d
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-08-08 13:25:46 +0200

    Add conctract test case

---
+++ src/word/domain/stored_word_repository.py
@@ -0,0 +1,20 @@
+import abc
+from typing import Optional
+from src.word.domain.word import Word
+from src.word.domain.stored_word import StoredWord
+from src.word.domain.stored_word_factory import StoredWordFactory
+
+class StoredWordRepository(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    async def find(
+        self,
+        word: Word,
+    ) -> Optional[StoredWord]:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    async def save(
+        self,
+        stored_word: StoredWord,
+    ) -> None:
+        raise NotImplementedError
