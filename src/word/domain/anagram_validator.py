07a1e7c91574b9a7c1b71c9b45e71b44b8a0504d
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-08-08 12:35:42 +0200

    Add type hints

--- src/word/domain/anagram_validator.py
+++ src/word/domain/anagram_validator.py
@@ -10,7 +10,7 @@ class AnagramValidator:
         left_word: StoredWord,
         right_word: StoredWord,
     ) -> bool:
-        self_frequency: Counter = Counter(left_word.word)
-        other_frequency: Counter = Counter(right_word.word)
+        self_frequency: Counter[str] = Counter(left_word.word)
+        other_frequency: Counter[str] = Counter(right_word.word)
 
         return self_frequency == other_frequency and left_word.word != right_word.word
