24f112e73d058dc56b54fc868db7f7b1eb46f44a
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-08-11 14:34:03 +0200

    Add word not found test

--- tests/word/application/test_update_word_use_case.py
+++ tests/word/application/test_update_word_use_case.py
@@ -1,5 +1,7 @@
 import pytest
 
+from src.shared.application.application_exceptions import ApplicationException
+from src.shared.domain.exceptions import NOT_FOUND
 from src.word.application.update_word_use_case import (
     UpdateWordUseCase,
 )
@@ -24,3 +26,17 @@ class TestUpdateWordUseCase:
         expected_response: StoredWordDto = StoredWordDto(word="pepe", position=8)
         
         assert response == expected_response
+
+    @pytest.mark.asyncio
+    async def test_update_non_stored_word(self) -> None:
+        stored_word_repository: StoredWordMockRepository = StoredWordMockRepository()
+        update_word_use_case: UpdateWordUseCase = UpdateWordUseCase(
+            stored_word_repository,
+        )
+        with pytest.raises(ApplicationException) as application_exception:
+            await update_word_use_case.update(
+                "calabozo",
+                8,
+            )
+        assert application_exception.value.exception_message == "calabozo not found"
+        assert application_exception.value.standard_exception == NOT_FOUND
