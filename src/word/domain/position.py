f662c465e28906d4e180ee7f9c02ef9772de0c14
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-08-11 11:25:46 +0200

    Add __lt__

--- src/word/domain/position.py
+++ src/word/domain/position.py
@@ -1,8 +1,11 @@
+from __future__ import annotations
+from typing import Union
+
 from pydantic import validator
 from pydantic.dataclasses import dataclass
 
 from src.shared.domain.domain_exceptions import DomainException
-from src.shared.domain.exceptions import INVALID_FIELD
+from src.shared.domain.exceptions import CONFLICT, INVALID_FIELD
 
 @dataclass(frozen=True)
 class Position:
@@ -20,3 +23,15 @@ class Position:
                 "Position must be a positive integer"
             )
         return position
+
+    def __lt__(
+        self,
+        other: Position,
+    ) -> bool:
+        if not isinstance(other, Position):
+            raise DomainException(
+                "Position",
+                INVALID_FIELD,
+                "Position cannot be compared",
+            )
+        return self.position < other.position 
