58d365216b360a094bac16e5859ab938e851ec4b
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-08-12 11:05:04 +0200

    Add delete word use case

---
+++ src/word/application/delete_word_use_case.py
@@ -0,0 +1,28 @@
+from src.shared.application.application_exceptions import (
+    ApplicationException,
+)
+from src.shared.domain.domain_exceptions import DomainException
+from src.word.domain.stored_word_repository import (
+    StoredWordRepository,
+)
+from ..domain.word import Word
+
+class DeleteWordUseCase:
+    def __init__(
+        self,
+        stored_word_repository: StoredWordRepository,
+    ) -> None:
+        self.__stored_word_repository = stored_word_repository
+
+    async def delete(
+        self,
+        word: str,
+    ) -> None:
+        try:
+            await self.__stored_word_repository.delete(Word(word))
+        except DomainException as domain_exception:
+            raise ApplicationException(
+                domain_exception.standard_exception,
+                domain_exception.exception_message,
+            )
+
