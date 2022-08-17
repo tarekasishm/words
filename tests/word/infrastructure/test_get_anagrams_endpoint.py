f9dfb4f1e73f0e2e053abfaf8ce833fc25cb0e55
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-08-17 09:00:14 +0200

    Add anagrams endpoint test cases

---
+++ tests/word/infrastructure/test_get_anagrams_endpoint.py
@@ -0,0 +1,40 @@
+from typing import Dict, List
+from tests.conftest import data_test, test_client
+
+
+class TestGetAnagramsEndpoint:
+    def test_get_anagrams_succesfully(
+        self,
+        populate_mongo: None,
+    ) -> None:
+        response = test_client.get(f"/api/v1/words/asco/anagrams")
+        assert response.status_code == 200
+
+        expected_response: Dict[str, List[str]] = {
+            "data": ["cosa", "caso"]
+        }
+        assert response.json() == expected_response
+
+    def test_not_found_anagrams(
+        self,
+        populate_mongo: None,
+    ) -> None:
+        response = test_client.get(f"/api/v1/words/carro/anagrams")
+        assert response.status_code == 200
+
+        expected_response: Dict[str, List[str]] = {
+            "data": []
+        }
+        assert response.json() == expected_response
+
+    def test_get_anagrams_without_including_same_word(
+        self,
+        populate_mongo: None,
+    ) -> None:
+        response = test_client.get(f"/api/v1/words/cosa/anagrams")
+        assert response.status_code == 200
+
+        expected_response: Dict[str, List[str]] = {
+            "data": ["caso"]
+        }
+        assert response.json() == expected_response
