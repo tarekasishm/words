13464f9ec893f59fc38ecdc09d248b78250c4054
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-08-21 15:50:24 +0200

    Fix transaction model

--- src/word/infrastructure/persistance/stored_word_mongo_repository.py
+++ src/word/infrastructure/persistance/stored_word_mongo_repository.py
@@ -17,6 +17,8 @@ from src.shared.domain.domain_exceptions import DomainException
 from src.shared.domain.exceptions import DEPENDENCY_PROBLEM, NOT_FOUND
 from src.shared.settings import Settings
 
+from time import perf_counter_ns
+
 
 class StoredWordMongoRepository(StoredWordRepository):
     def __init__(
@@ -90,15 +92,13 @@ class StoredWordMongoRepository(StoredWordRepository):
         self,
         stored_word: StoredWord,
     ) -> StoredWord:
-        async with self.__session.start_transaction():
-            return await self._run_transaction_with_retry(self._save, stored_word)
+        return await self._run_transaction_with_retry(self._save, stored_word)
 
     async def update(
         self,
         stored_word: StoredWord,
     ) -> StoredWord:
-        async with self.__session.start_transaction():
-            return await self._run_transaction_with_retry(self._update, stored_word)
+        return await self._run_transaction_with_retry(self._update, stored_word)
 
     async def delete(
         self,
@@ -112,8 +112,7 @@ class StoredWordMongoRepository(StoredWordRepository):
                     NOT_FOUND,
                     f"{word.word} not found",
                 )
-            async with self.__session.start_transaction():
-                await self._run_transaction_with_retry(self._delete, current_word)
+            await self._run_transaction_with_retry(self._delete, current_word)
         except DomainException as domain_exception:
             raise domain_exception
         except Exception:
@@ -129,9 +128,9 @@ class StoredWordMongoRepository(StoredWordRepository):
     ) -> List[StoredWord]:
         try:
             anagrams: List[StoredWord] = []
-            async for stored_word_dict in self.__session.client[ 
-                self.__words_database
-            ][self.__words_collection].find({}, session=self.__session):
+            async for stored_word_dict in self.__session.client[self.__words_database][
+                self.__words_collection
+            ].find({}, session=self.__session):
                 if AnagramValidator.is_anagram(word, Word(stored_word_dict["_id"])):
                     anagrams.append(
                         StoredWordFactory.build(
@@ -152,26 +151,26 @@ class StoredWordMongoRepository(StoredWordRepository):
         current_word: StoredWord,
     ) -> StoredWord:
         try:
-
-            delete_result: DeleteResult = await self.__session.client[
-                self.__words_database
-            ][self.__words_collection].delete_one(
-                {"_id": current_word.word}, session=self.__session
-            )
-            if delete_result.deleted_count != 1:
-                raise DomainException(
-                    "StoredWordMongoRepository",
-                    NOT_FOUND,
-                    f"{current_word.word} could not be deleted",
+            async with self.__session.start_transaction():
+                delete_result: DeleteResult = await self.__session.client[
+                    self.__words_database
+                ][self.__words_collection].delete_one(
+                    {"_id": current_word.word}, session=self.__session
                 )
-            await self.__session.client[self.__words_database][
-                self.__words_collection
-            ].update_many(
-                {"position": {"$gt": current_word.position}},
-                {"$inc": {"position": -1}},
-                session=self.__session,
-            )
-            return current_word
+                if delete_result.deleted_count != 1:
+                    raise DomainException(
+                        "StoredWordMongoRepository",
+                        NOT_FOUND,
+                        f"{current_word.word} could not be deleted",
+                    )
+                await self.__session.client[self.__words_database][
+                    self.__words_collection
+                ].update_many(
+                    {"position": {"$gt": current_word.position}},
+                    {"$inc": {"position": -1}},
+                    session=self.__session,
+                )
+                return current_word
         except DomainException as domain_exception:
             raise domain_exception
         except Exception:
@@ -186,112 +185,118 @@ class StoredWordMongoRepository(StoredWordRepository):
         new_stored_word: StoredWord,
     ) -> StoredWord:
         # ToDo: check returned values
-        current_word: Optional[StoredWord] = await self.find(Word(new_stored_word.word))
-        if current_word is None:
-            raise DomainException(
-                "StoredWordMongoRepostiory",
-                NOT_FOUND,
-                f"{new_stored_word.word} not found",
+        async with self.__session.start_transaction():
+            current_word: Optional[StoredWord] = await self.find(
+                Word(new_stored_word.word)
             )
-        if current_word == new_stored_word:
-            return current_word
-        inserted_position: Position = await self._select_for_update_last_word(
-            new_stored_word
-        )
-        inserted_position = (
-            inserted_position
-            if inserted_position.position == new_stored_word.position
-            else Position(inserted_position.position - 1)
-        )
-        query: Dict[str, Any] = {
-            "position": {
-                "$gt": current_word.position,
-                "$lte": inserted_position.position,
-            }
-        }
-        update: Dict[str, Any] = {"$inc": {"position": -1}}
-        if inserted_position.position < current_word.position:
-            query = {
+            if current_word is None:
+                raise DomainException(
+                    "StoredWordMongoRepostiory",
+                    NOT_FOUND,
+                    f"{new_stored_word.word} not found",
+                )
+            if current_word == new_stored_word:
+                return current_word
+            inserted_position: Position = await self._select_for_update_last_word(
+                new_stored_word
+            )
+            inserted_position = (
+                inserted_position
+                if inserted_position.position == new_stored_word.position
+                else Position(inserted_position.position - 1)
+            )
+            query: Dict[str, Any] = {
                 "position": {
-                    "$gte": inserted_position.position,
-                    "$lte": current_word.position,
+                    "$gt": current_word.position,
+                    "$lte": inserted_position.position,
                 }
             }
-            update = {"$inc": {"position": 1}}
-        await self.__session.client[self.__words_database][
-            self.__words_collection
-        ].update_many(query, update, session=self.__session)
-        await self.__session.client[self.__words_database][
-            self.__words_collection
-        ].update_one(
-            {"_id": new_stored_word.word},
-            {"$set": {"position": inserted_position.position}},
-            session=self.__session,
-        )
-        return StoredWordFactory.build(new_stored_word.word, inserted_position.position)
+            update: Dict[str, Any] = {"$inc": {"position": -1}}
+            if inserted_position.position < current_word.position:
+                query = {
+                    "position": {
+                        "$gte": inserted_position.position,
+                        "$lte": current_word.position,
+                    }
+                }
+                update = {"$inc": {"position": 1}}
+            await self.__session.client[self.__words_database][
+                self.__words_collection
+            ].update_many(query, update, session=self.__session)
+            await self.__session.client[self.__words_database][
+                self.__words_collection
+            ].update_one(
+                {"_id": new_stored_word.word},
+                {"$set": {"position": inserted_position.position}},
+                session=self.__session,
+            )
+            return StoredWordFactory.build(
+                new_stored_word.word, inserted_position.position
+            )
 
     async def _save(
         self,
         stored_word: StoredWord,
     ) -> StoredWord:
-        last_position: Optional[Position] = None
-        last_word: Optional[Word] = None
-        async for last_stored_word in self.__session.client[self.__words_database][
-            self.__words_collection
-        ].find({}, session=self.__session).sort("position", -1).limit(1):
-            last_position = Position(last_stored_word["position"])
-            last_word = Word(last_stored_word["_id"])
+        async with self.__session.start_transaction():
+            last_position: Optional[Position] = None
+            last_word: Optional[Word] = None
+            async for last_stored_word in self.__session.client[self.__words_database][
+                self.__words_collection
+            ].find({}, session=self.__session).sort("position", -1).limit(1):
+                last_position = Position(last_stored_word["position"])
+                last_word = Word(last_stored_word["_id"])
 
-        # select for update
-        if last_word:
+            # select for update
+            if last_word:
+                await self.__session.client[self.__words_database][
+                    self.__words_collection
+                ].find_one_and_update(
+                    {
+                        "_id": last_word.word,
+                    },
+                    {"$set": {"Lock": ObjectId()}},
+                    session=self.__session,
+                )
+            inserted_position: Position = Position(stored_word.position)
+            if last_position is None:
+                inserted_position = Position(1)
+            if last_position and inserted_position > last_position:
+                inserted_position = Position(last_position.position + 1)
             await self.__session.client[self.__words_database][
                 self.__words_collection
-            ].find_one_and_update(
-                {
-                    "_id": last_word.word,
-                },
-                {"$set": {"Lock": ObjectId()}},
+            ].insert_one(
+                {"_id": stored_word.word, "position": inserted_position.position},
                 session=self.__session,
             )
-        inserted_position: Position = Position(stored_word.position)
-        if last_position is None:
-            inserted_position = Position(1)
-        if last_position and inserted_position > last_position:
-            inserted_position = Position(last_position.position + 1)
-        await self.__session.client[self.__words_database][
-            self.__words_collection
-        ].insert_one(
-            {"_id": stored_word.word, "position": inserted_position.position},
-            session=self.__session,
-        )
-        if last_position and inserted_position <= last_position:
-            await self.__session.client[self.__words_database][
-                self.__words_collection
-            ].update_many(
-                {
-                    "position": {"$gte": inserted_position.position},
-                    "_id": {"$ne": stored_word.word},
-                },
-                {"$inc": {"position": 1}},
-                session=self.__session,
-            )
-        return StoredWordFactory.build(stored_word.word, inserted_position.position)
+            if last_position and inserted_position <= last_position:
+                await self.__session.client[self.__words_database][
+                    self.__words_collection
+                ].update_many(
+                    {
+                        "position": {"$gte": inserted_position.position},
+                        "_id": {"$ne": stored_word.word},
+                    },
+                    {"$inc": {"position": 1}},
+                    session=self.__session,
+                )
+            return StoredWordFactory.build(stored_word.word, inserted_position.position)
 
     async def _run_transaction_with_retry(
         self,
         txn_coro: Callable[[StoredWord], Coroutine[Any, Any, StoredWord]],
         stored_word: StoredWord,
     ) -> StoredWord:
+        counter: int = 1
         while True:
             try:
+                print(f"iteration {counter}")
+                counter += 1
                 real_stored_word: StoredWord = await txn_coro(
                     stored_word,
                 )  # performs transaction
                 return real_stored_word
             except (ConnectionFailure, OperationFailure) as exc:
-                import traceback
-
-                traceback.print_exc()
                 # If transient error, retry the whole transaction
                 if exc.has_error_label("TransientTransactionError"):
                     continue
@@ -301,9 +306,6 @@ class StoredWordMongoRepository(StoredWordRepository):
                     "Service not available. Please try later.",
                 )
             except Exception:
-                import traceback
-
-                traceback.print_exc()
                 raise DomainException(
                     "StoredWordRepository",
                     DEPENDENCY_PROBLEM,
