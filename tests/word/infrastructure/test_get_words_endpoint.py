9ca2fac719354a6cc32e27ddc5d1ae9794ba8297
Tarek-Asis Hamed Mohatar <tarekasishm@gmail.com>
2022-08-22 09:47:31 +0200

    condition running

--- tests/word/infrastructure/test_get_words_endpoint.py
+++ tests/word/infrastructure/test_get_words_endpoint.py
@@ -1,17 +1,24 @@
+import os
+
+import pytest
+
 from tests.conftest import data_test, test_client
 
 
 class TestGetWordsEndpoint:
+    @pytest.mark.skipif( not os.getenv("RUN_VALIDATION_TEST", False), reason="Environment not ready" )
     def test_get_all_words(self, populate_mongo: None) -> None:
         response = test_client.get("/api/v1/words", params={"limit": 10, "offset": 0})
         assert response.status_code == 200
         assert response.json()["data"] == data_test
 
+    @pytest.mark.skipif( not os.getenv("RUN_VALIDATION_TEST", False), reason="Environment not ready" )
     def test_get_paginated_words(self, populate_mongo: None) -> None:
         response = test_client.get("/api/v1/words", params={"limit": 3, "offset": 2})
         assert response.status_code == 200
         assert response.json()["data"] == data_test[2:5]
 
+    @pytest.mark.skipif( not os.getenv("RUN_VALIDATION_TEST", False), reason="Environment not ready" )
     def test_invalid_offset_value(
         self,
         populate_mongo: None,
@@ -23,6 +30,7 @@ class TestGetWordsEndpoint:
             response.json()["message"] == "Offset must be greater than or equal to zero"
         )
 
+    @pytest.mark.skipif( not os.getenv("RUN_VALIDATION_TEST", False), reason="Environment not ready" )
     def test_invalid_limit_value(
         self,
         populate_mongo: None,
